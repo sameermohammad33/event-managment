f3e69993c54fc2c3469485324aeb99fe443e8645
sameermohammad33 <Sameer.Mohammad@cognizant.com>
2025-04-17 12:21:55 +0530

    Create app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { provideHttpClient } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [provideHttpClient()]
+})
+export class AppRoutingModule { }
